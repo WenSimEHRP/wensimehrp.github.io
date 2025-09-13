2106f058b46853f8a07ad03ce37a8136249c3907
Jeremy Gao <wensimehrp@gmail.com>
2025-09-12 21:56:27 -0700

    feat: try fix typst font problem

--- shell.nix
+++ shell.nix
@@ -6,10 +6,21 @@ with pkgs;
 
 mkShell rec {
   nativeBuildInputs = [
+    fontconfig
     bun
     typst
     # font section
     merriweather
     merriweather-sans
+    sarasa-gothic
   ];
+  shellHook = ''
+      export FONTCONFIG_FILE=${makeFontsConf {
+      fontDirectories = [
+        merriweather
+        merriweather-sans
+        sarasa-gothic
+      ];
+    }};
+  '';
 }
